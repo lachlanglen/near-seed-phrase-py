db4e9586e4a6817d8121603c081bb140ebc311a5
lachlanglen <lachlanjglen@gmail.com>
2022-11-20 14:56:57 -0700

    debug hmac

--- index.py
+++ index.py
@@ -3,9 +3,12 @@
 from mnemonic import Mnemonic
 import ed25519
 import base58
+import base64
 import hmac
 import hashlib
 from functools import reduce
+
+import near_api
 
 KEY_DERIVATION_PATH = "m/44'/397'/0'"
 HARDENED_OFFSET = 0x80000000
@@ -24,14 +27,31 @@ def CDKPriv(parent_keys, index: int):
         "chain_code": IR
     }
 
+def hmac_sig(key):
+    hmac_sig = hmac.new(key, msg=None, digestmod=hashlib.sha512)
+    hmac_sig = hmac_sig.digest()
+    return hmac_sig
+
 def get_master_key_from_seed(seed: str):
-    H = hmac.new(key=seed, digestmod=hashlib.sha512).digest()
+    # seed = bytes('hello', 'utf-8')
+    print('seed len: ', len(seed))
+    print("seed: ", seed)
+    # print("seed hex len: ", len(seed.hex()))
+    H = hmac.new(key=bytes('ed25519 seed', 'utf-8'), msg=bytearray.fromhex(seed), digestmod=hashlib.sha512)
+    print("H hex: ", H.digest().hex())
+    # H.update(bytes.fromhex(seed.hex()))
+    H.update(bytes('hi', 'utf-8'))
+    # H = hmac.new(key=bytes('ed25519 seed', 'utf-8') + seed, digestmod=hashlib.sha512).digest()
+    print("H hex 2: ", H.hexdigest())
     key = H[:32]
     chain_code = H[32:]
     return (key, chain_code)
 
 def derive_path(path: str, seed: str):
     (key, chain_code) = get_master_key_from_seed(seed)
+    print("DERIVE PATH")
+    print("dp key hex: ", key.hex())
+    print("dp chain_code hex: ", chain_code.hex())
     segments = path.split("/")[1:]
     segments_list = list(map(lambda x: int(x.replace("'", '')), segments))
     return reduce(CDKPriv, segments_list, { "key": key, "chain_code": chain_code })
@@ -48,7 +68,8 @@ def base_58_key(key: ed25519.SigningKey or ed25519.VerifyingKey):
 
 def parse_seed_phrase(seed_phrase: str, derivation_path:str=KEY_DERIVATION_PATH):
     seed = mnemo.to_seed(normalize_seed_phrase(seed_phrase))
-    key = derive_path(derivation_path, seed)["key"]
+    key = derive_path(derivation_path, seed.hex())["key"]
+    print("hex derived path key: ", key.hex())
     signing_key = ed25519.SigningKey(key)
     signing_key_58 = base_58_key(signing_key)
     secret_key = f"ed25519:{signing_key_58}"
@@ -60,3 +81,19 @@ def parse_seed_phrase(seed_phrase: str, derivation_path:str=KEY_DERIVATION_PATH)
         "secret_key": secret_key,
         "public_key": public_key,
     }
+
+parsed = parse_seed_phrase("jacket blame unhappy disorder expand account frozen slide rival feature grief space lemon garbage pride huge antique century")
+print("parsed: ", parsed)
+
+public_key = parsed["public_key"]
+print("hex: ", base58.b58decode(public_key.replace("ed25519:", "")).hex())
+
+near_provider = near_api.providers.JsonProvider("https://rpc.testnet.near.org")
+
+sender_key_pair = near_api.signer.KeyPair(parsed["secret_key"])
+sender_signer = near_api.signer.Signer("lachlan100.testnet", sender_key_pair)
+sender_account = near_api.account.Account(near_provider, sender_signer, "lachlan100.testnet")
+
+out = sender_account.send_money("lachlan-nft-test-2.testnet", 1000)
+
+print(out)
