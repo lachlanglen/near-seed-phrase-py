b6a3782622958648b698978e9f18858ef3e83647
lachlanglen <lachlanjglen@gmail.com>
2022-11-22 11:20:57 -0700

    move from ed25519 to pynacl

--- near_seed_phrase/main.py
+++ near_seed_phrase/main.py
@@ -1,8 +1,8 @@
 from mnemonic import Mnemonic
-import ed25519
-import base58
+from nacl.signing import SigningKey
+from base58 import b58decode
 from near_seed_phrase.utils.hd_key import derive_path
-from near_seed_phrase.utils.near_keys import format_ed25519_key, base_58_key
+from near_seed_phrase.utils.near_keys import format_ed25519_key, base_58_signing_key, base_58_verify_key
 from near_seed_phrase.utils.seed_phrase import normalize_seed_phrase
 
 KEY_DERIVATION_PATH = "m/44'/397'/0'"
@@ -10,19 +10,19 @@ HARDENED_OFFSET = 0x80000000
 
 mnemo = Mnemonic("english")
 
-
 def parse_seed_phrase(seed_phrase: str, derivation_path:str=KEY_DERIVATION_PATH):
     """ Parses NEAR credentials from bip39 seed phrase """
     seed = mnemo.to_seed(normalize_seed_phrase(seed_phrase))
     (key, _) = derive_path(derivation_path, seed.hex())
-    signing_key_raw = ed25519.SigningKey(key)
-    secret_key = format_ed25519_key(base_58_key(signing_key_raw))
-    public_key = format_ed25519_key(base_58_key(signing_key_raw.get_verifying_key()))
+    signing_key = SigningKey(key)
+    verify_key = signing_key.verify_key
+    secret_key = format_ed25519_key(base_58_signing_key(signing_key))
+    public_key = format_ed25519_key(base_58_verify_key(verify_key))
     return {
         "seed_phrase": seed_phrase,
         "secret_key": secret_key,
         "public_key": public_key,
-        "public_key_hex": base58.b58decode(public_key.replace("ed25519:", "")).hex()
+        "public_key_hex": b58decode(public_key.replace("ed25519:", "")).hex()
     }
 
 
--- near_seed_phrase/utils/near_keys.py
+++ near_seed_phrase/utils/near_keys.py
@@ -1,9 +1,13 @@
-import ed25519
-import base58
+from nacl.signing import SigningKey, VerifyKey
+from base58 import b58encode
 
-def base_58_key(key: ed25519.SigningKey or ed25519.VerifyingKey):
-    """Converts a key to base58"""
-    return base58.b58encode(key.to_bytes()).decode('utf-8')
+def base_58_signing_key(key: SigningKey):
+    """Converts a signing key to base58"""
+    return b58encode(key._signing_key).decode('utf-8')
+
+def base_58_verify_key(key: VerifyKey):
+    """Converts a verify key to base58"""
+    return b58encode(key.encode()).decode('utf-8')
 
 def format_ed25519_key(key: str):
     """Formats ed25519 key for NEAR PROTOCOL"""
