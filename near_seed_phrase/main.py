655535f9f6e1436a49020c76a1ddd744c0ac35ef
lachlanglen <lachlanjglen@gmail.com>
2022-11-22 11:38:33 -0700

    refactor base_58_key_from_bytes

--- near_seed_phrase/main.py
+++ near_seed_phrase/main.py
@@ -2,7 +2,7 @@ from mnemonic import Mnemonic
 from nacl.signing import SigningKey
 from base58 import b58decode
 from near_seed_phrase.utils.hd_key import derive_path
-from near_seed_phrase.utils.near_keys import format_ed25519_key, base_58_signing_key, base_58_verify_key
+from near_seed_phrase.utils.near_keys import format_ed25519_key, base_58_key_from_bytes
 from near_seed_phrase.utils.seed_phrase import normalize_seed_phrase
 
 KEY_DERIVATION_PATH = "m/44'/397'/0'"
@@ -16,8 +16,8 @@ def parse_seed_phrase(seed_phrase: str, derivation_path:str=KEY_DERIVATION_PATH)
     (key, _) = derive_path(derivation_path, seed.hex())
     signing_key = SigningKey(key)
     verify_key = signing_key.verify_key
-    secret_key = format_ed25519_key(base_58_signing_key(signing_key))
-    public_key = format_ed25519_key(base_58_verify_key(verify_key))
+    secret_key = format_ed25519_key(base_58_key_from_bytes(signing_key._signing_key))
+    public_key = format_ed25519_key(base_58_key_from_bytes(verify_key.encode()))
     return {
         "seed_phrase": seed_phrase,
         "secret_key": secret_key,
--- near_seed_phrase/utils/near_keys.py
+++ near_seed_phrase/utils/near_keys.py
@@ -1,13 +1,8 @@
-from nacl.signing import SigningKey, VerifyKey
 from base58 import b58encode
 
-def base_58_signing_key(key: SigningKey):
+def base_58_key_from_bytes(bytes: bytes):
     """Converts a signing key to base58"""
-    return b58encode(key._signing_key).decode('utf-8')
-
-def base_58_verify_key(key: VerifyKey):
-    """Converts a verify key to base58"""
-    return b58encode(key.encode()).decode('utf-8')
+    return b58encode(bytes).decode('utf-8')
 
 def format_ed25519_key(key: str):
     """Formats ed25519 key for NEAR PROTOCOL"""
