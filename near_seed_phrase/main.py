613b70868cc24f67528e658327665a9877acd616
lachlanglen <lachlanjglen@gmail.com>
2022-11-21 17:17:35 -0700

    fix imports

--- near_seed_phrase/main.py
+++ near_seed_phrase/main.py
@@ -1,7 +1,9 @@
 from mnemonic import Mnemonic
 import ed25519
 import base58
-from utils import derive_path, normalize_seed_phrase, base_58_key, format_ed25519_key
+from utils import hd_key
+from utils import near_keys
+from utils import seed_phrase as seed_phrase_utils
 
 KEY_DERIVATION_PATH = "m/44'/397'/0'"
 HARDENED_OFFSET = 0x80000000
@@ -9,14 +11,13 @@ HARDENED_OFFSET = 0x80000000
 mnemo = Mnemonic("english")
 
 
-
 def parse_seed_phrase(seed_phrase: str, derivation_path:str=KEY_DERIVATION_PATH):
     """ Parses NEAR credentials from bip39 seed phrase """
-    seed = mnemo.to_seed(normalize_seed_phrase(seed_phrase))
-    (key, _) = derive_path(derivation_path, seed.hex())
+    seed = mnemo.to_seed(seed_phrase_utils.normalize_seed_phrase(seed_phrase))
+    (key, _) = hd_key.derive_path(derivation_path, seed.hex())
     signing_key_raw = ed25519.SigningKey(key)
-    secret_key = format_ed25519_key(base_58_key(signing_key_raw))
-    public_key = format_ed25519_key(base_58_key(signing_key_raw.get_verifying_key()))
+    secret_key = near_keys.format_ed25519_key(near_keys.base_58_key(signing_key_raw))
+    public_key = near_keys.format_ed25519_key(near_keys.base_58_key(signing_key_raw.get_verifying_key()))
     return {
         "seed_phrase": seed_phrase,
         "secret_key": secret_key,
--- near_seed_phrase/utils/__init__.py
+++ near_seed_phrase/utils/__init__.py
@@ -1,3 +1,6 @@
-from .hd_key import derive_path
-from .seed_phrase import normalize_seed_phrase
-from .near_keys import base_58_key, format_ed25519_key
+# from .hd_key import derive_path
+# from .seed_phrase import normalize_seed_phrase
+# from .near_keys import base_58_key, format_ed25519_key
+import utils.hd_key
+import utils.near_keys
+import utils.seed_phrase
